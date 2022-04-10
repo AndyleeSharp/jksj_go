ebb67fcabe07e756efcfc6d0e57fa31dd0f2cf39
andy lee <andy-lee@qq.com>
2022-04-10 11:43:27 +0800

    增加make build逻辑；添加信号

--- week04/app/shop/interface/cmd/server/main.go
+++ week04/app/shop/interface/cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"syscall"
+
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
@@ -13,6 +15,7 @@ func newApp(hs *http.Server, gs *grpc.Server) *kratos.App {
 			hs,
 			gs,
 		),
+		kratos.Signal(syscall.SIGINT, syscall.SIGTERM),
 	)
 }
 func main() {
